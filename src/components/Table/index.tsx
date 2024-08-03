634e041d477c7f23807931c46282d38a0371bf3b
athena272 <guilhermera272@gmail.com>
2024-08-03 13:33:16 -0300

    Update index.tsx

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,10 +1,31 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Paper, Table, TableBody, TableCell, tableCellClasses, TableContainer, TableHead, TableRow } from "@mui/material";
 import IConsulta from "../../types/IConsulta";
 import formatDate from "../../utils";
+import styled from "@emotion/styled";
 
 type MyTableProps = {
     consultas: IConsulta[] | null
 }
+
+const StyledCell = styled(TableCell)(() => ({
+    [`&.${tableCellClasses.head}`]: {
+        color: "var(--azul-escuro)",
+        fontSize: 18,
+        fontWeight: 700,
+        fontFamily: "var(--fonte-principal)"
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 16,
+        fontFamily: "var(--fonte-principal)"
+    }
+}))
+
+const StyledRow = styled(TableRow)(() => ({
+    [`&:nth-of-type(odd)`]: {
+        backgroundColor: "var(--cinza-claro)",
+        align: "right"
+    }
+}))
 
 export default function MyTable({ consultas }: MyTableProps) {
     return (
@@ -12,26 +33,26 @@ export default function MyTable({ consultas }: MyTableProps) {
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 700 }} aria-label="tabela-customizada">
                     <TableHead>
-                        <TableRow>
-                            <TableCell>Data</TableCell>
-                            <TableCell>Horário</TableCell>
-                            <TableCell>Profissional</TableCell>
-                            <TableCell>Especialidade</TableCell>
-                            <TableCell>Paciente</TableCell>
-                            <TableCell>Modalidade</TableCell>
-                        </TableRow>
+                        <StyledRow>
+                            <StyledCell>Data</StyledCell>
+                            <StyledCell>Horário</StyledCell>
+                            <StyledCell>Profissional</StyledCell>
+                            <StyledCell>Especialidade</StyledCell>
+                            <StyledCell>Paciente</StyledCell>
+                            <StyledCell>Modalidade</StyledCell>
+                        </StyledRow>
                     </TableHead>
                     <TableBody>
                         {
                             consultas?.map(consulta => (
-                                <TableRow key={consulta.id}>
-                                    <TableCell component="th" scope="row">{formatDate(consulta.data)}</TableCell>
-                                    <TableCell>{consulta.horario}</TableCell>
-                                    <TableCell>{consulta.profissional[0].nome}</TableCell>
-                                    <TableCell>{consulta.profissional[0].especialidade}</TableCell>
-                                    <TableCell>{consulta.paciente}</TableCell>
-                                    <TableCell>{consulta.modalidade}</TableCell>
-                                </TableRow>
+                                <StyledRow key={consulta.id}>
+                                    <StyledCell component="th" scope="row">{formatDate(consulta.data)}</StyledCell>
+                                    <StyledCell>{consulta.horario}</StyledCell>
+                                    <StyledCell>{consulta.profissional[0].nome}</StyledCell>
+                                    <StyledCell>{consulta.profissional[0].especialidade}</StyledCell>
+                                    <StyledCell>{consulta.paciente}</StyledCell>
+                                    <StyledCell>{consulta.modalidade}</StyledCell>
+                                </StyledRow>
                             ))
                         }
                     </TableBody>
