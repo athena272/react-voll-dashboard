1dfc0d26f3b44e8f17ca662959e8db6d2c70f340
athena272 <guilhermera272@gmail.com>
2024-08-02 23:19:47 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import Container from './components/Container';
 import Header from './components/Header';
 import Title from './components/Title';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Container>
         <Title>Área administrativa</Title>
       </Container>
+      <Footer />
     </>
   )
 }
