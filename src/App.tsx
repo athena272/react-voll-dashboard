33d2b36eaa63a4203238914d63a4254d0987dedf
athena272 <guilhermera272@gmail.com>
2024-08-03 13:15:57 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import Container from './components/Container';
 import Header from './components/Header';
 import Title from './components/Title';
 import Footer from './components/Footer';
+import MyTable from './components/Table';
+import useDadosConsulta from './hooks/useDadosConsulta';
 
 export default function App() {
+  const { dados, error } = useDadosConsulta()
+  console.log("🚀 ~ App ~ dados:", dados)
+
   return (
     <>
       <Header />
       <Container>
         <Title>Área administrativa</Title>
+        <MyTable consultas={dados}/>
       </Container>
       <Footer />
     </>
