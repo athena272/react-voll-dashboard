a14168fbbbb2fe663fe548a6f44ad48117555fab
athena272 <guilhermera272@gmail.com>
2024-08-03 21:12:47 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,26 @@ import Header from './components/Header';
 import Title from './components/Title';
 import Footer from './components/Footer';
 import MyTable from './components/Table';
+import Graphic from './components/Graphic';
 import useDadosConsulta from './hooks/useDadosConsulta';
+import useDadosProfissional from './hooks/useDadosProfissional';
 
 export default function App() {
-  const { dados, error } = useDadosConsulta()
-  console.log("🚀 ~ App ~ dados:", dados)
+  const { dados: consultas, error: consultasErro } = useDadosConsulta()
+  const { dados: profissionais, error: profissionaisErro } = useDadosProfissional()
+
+  if (consultasErro || profissionaisErro) {
+    console.log("Ocorreu um erro na requisição")
+    throw new Error("Ocorreu um erro na requisição")
+  }
 
   return (
     <>
       <Header />
       <Container>
         <Title>Área administrativa</Title>
-        <MyTable consultas={dados}/>
+        <MyTable consultas={consultas} />
+        <Graphic consultas={consultas} profissionais={profissionais} />
       </Container>
       <Footer />
     </>
