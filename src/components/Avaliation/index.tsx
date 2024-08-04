ed5cd8dbbdb63d8e15162d0caad0062e70e07e41
athena272 <guilhermera272@gmail.com>
2024-08-03 21:52:18 -0300

    Create index.tsx

---
+++ src/components/Avaliation/index.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components"
+import IProfissional from "../../types/IProfissional"
+import Card from "./Card"
+
+type AvaliationProps = {
+    profissionais: IProfissional[] | null
+}
+
+const CardSection = styled.section`
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+width: 100%;
+`
+
+export default function Avaliation({ profissionais }: AvaliationProps) {
+    return (
+        <CardSection>
+            {
+                profissionais?.map(profissional => (
+                    <Card key={profissional.id} profissional={profissional} />
+                ))
+            }
+        </CardSection>
+    )
+}
