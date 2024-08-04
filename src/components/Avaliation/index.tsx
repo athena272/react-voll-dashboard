e3176ca557113b07847214fa8eed3f9395afa150
athena272 <guilhermera272@gmail.com>
2024-08-03 22:42:48 -0300

    Update index.tsx

--- src/components/Avaliation/index.tsx
+++ src/components/Avaliation/index.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import IProfissional from "../../types/IProfissional"
 import Card from "./Card"
+import Button from "../Button"
 
 type AvaliationProps = {
     profissionais: IProfissional[] | null
@@ -15,12 +16,15 @@ width: 100%;
 
 export default function Avaliation({ profissionais }: AvaliationProps) {
     return (
-        <CardSection>
-            {
-                profissionais?.map(profissional => (
-                    <Card key={profissional.id} profissional={profissional} />
-                ))
-            }
-        </CardSection>
+        <>
+            <CardSection>
+                {
+                    profissionais?.map(profissional => (
+                        <Card key={profissional.id} profissional={profissional} />
+                    ))
+                }
+            </CardSection>
+            <Button>Ver mais</Button>
+        </>
     )
 }
